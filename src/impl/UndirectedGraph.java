79680f0e8d78938941bb9bf661767eb106f77687
GuillaumeClementPolytech <43247385+GuillaumeClementPolytech@users.noreply.github.com>
2019-05-24 12:44:07 +0200

    Add files via upload

---
+++ src/impl/UndirectedGraph.java
@@ -0,0 +1,39 @@
+package impl;
+
+import graph.*;
+
+public class UndirectedGraph implements Graph{
+
+	private Edge[] edges;
+	private Vertex[] vertices;
+
+	public void addEdge(Edge e){
+
+	}
+	
+	public void addVertex(Vertex vertex){
+
+	}
+
+	public void deleteEdge(Edge e){
+
+	}
+
+	public void deleteVertex(Vertex v){
+
+	}
+
+	public Vertex getVertexById(int id){
+
+	}
+
+	public Edge getEdgeById(int id){
+
+	}
+
+	public Vertex[] getNeighbours(int id){
+
+	}
+
+
+}
